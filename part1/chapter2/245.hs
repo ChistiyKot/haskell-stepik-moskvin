843a9f7fe99c7fa9832de5cabaaa246eb681c93c
chistiykot <chistiykot1@yandex.ru>
2021-04-18 10:01:31 +0300

    fix solution #1.2.4.5

--- part1/chapter2/245.hs
+++ part1/chapter2/245.hs
@@ -1,10 +1,10 @@
-ip = show a ++ show b ++ show c ++ show d
+-- ip = show a ++ show b ++ show c ++ show d
 
-data Ip = Ip Int
-instance Show Ip where
-    show (Ip a) = show a ++ "."
+data IpPart = IpPart Int
+instance Show IpPart where
+    show (IpPart a) = show a ++ "."
 
-a = Ip 127
-b = Ip 224
-c = Ip 120
-d = 0.12
+a = IpPart 127
+b = IpPart 224
+c = IpPart 120
+d = 12
