9db8793b545c4286d8f4046892b2580698ddae90
Chistiy Kot <ChistiyKot1@yandex.ru>
2021-05-08 10:25:57 +0300

    solution #1.4.4.3

---
+++ part1/chapter4/443.hs
@@ -0,0 +1,7 @@
+data Coord a = Coord a a
+
+distance :: Coord Double -> Coord Double -> Double
+distance (Coord x1 y1) (Coord x2 y2) = sqrt $ (x1 - x2)**2 + (y1 - y2)**2
+
+manhDistance :: Coord Int -> Coord Int -> Int
+manhDistance (Coord p1 q1) (Coord p2 q2) = (abs $ p1 - q1) + (abs $ p2 - q2)
