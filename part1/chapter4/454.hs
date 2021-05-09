7e5eea9343cac2612eb4e98673bf7e5c9eb056da
chistiykot <chistiykot1@yandex.ru>
2021-05-09 11:55:29 +0300

    solution #1.4.5.4 update

--- part1/chapter4/454.hs
+++ part1/chapter4/454.hs
@@ -19,3 +19,26 @@ fac a = toNat $ facN $ fromNat a
 toNat :: Integer -> Nat
 toNat 0 = Zero
 toNat n = Suc (toNat $ n - 1)
+
+
+{-
+-- более красивое решение без преобразований (не мое)
+
+data Nat = Zero | Suc Nat
+
+fromNat :: Nat -> Integer
+fromNat Zero = 0
+fromNat (Suc n) = fromNat n + 1
+
+add :: Nat -> Nat -> Nat
+add Zero x = x
+add (Suc x) y = add x (Suc y)
+
+mul :: Nat -> Nat -> Nat
+mul Zero _ = Zero
+mul (Suc x) y = add (mul x y) y
+
+fac :: Nat -> Nat
+fac Zero = Suc Zero
+fac n1@(Suc n) = mul n1 (fac n)
+-}
